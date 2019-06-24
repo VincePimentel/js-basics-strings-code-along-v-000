ce88a4dd678154c1224ab26606419bbc193748dd
Vince Andrew Pimentel <contact@vincep.me>
2019-06-24 17:18:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const currentUser = "Mao";
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
-const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
